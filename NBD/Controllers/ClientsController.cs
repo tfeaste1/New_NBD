677d3bded1cd50700dda7d260359d2328fa52dba
Torika Feaste <34326254+tfeaste1@users.noreply.github.com>
2020-03-11 14:24:29 -0400

    push

--- NBD/Controllers/ClientsController.cs
+++ NBD/Controllers/ClientsController.cs
@@ -123,35 +123,7 @@ namespace NBD.Controllers
             return View(client);
         }
 
-        // GET: Clients/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var client = await _context.Clients
-                .Include(c => c.City)
-                .FirstOrDefaultAsync(m => m.ID == id);
-            if (client == null)
-            {
-                return NotFound();
-            }
-
-            return View(client);
-        }
-
-        // POST: Clients/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var client = await _context.Clients.FindAsync(id);
-            _context.Clients.Remove(client);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+       
 
         private void PopulateDropDownLists(Client client = null)
         {
